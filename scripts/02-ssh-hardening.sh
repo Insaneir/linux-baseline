216926b01720affe3eb57c251fa4cee7fd20631b
Insaneir <insaneir@gmail.com>
2025-12-16 11:29:21 +0700

    Update 02-ssh-hardening.sh

--- scripts/02-ssh-hardening.sh
+++ scripts/02-ssh-hardening.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-SSHD="/etc/ssh/sshd_config"
+set -e
 
-sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' $SSHD
-sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication no/' $SSHD
+SSHD="/etc/ssh/sshd_config"
+BACKUP="/etc/ssh/sshd_config.bak"
+
+# Backup ก่อนเสมอ
+[ ! -f "$BACKUP" ] && cp $SSHD $BACKUP
+
+apply() {
+  key="$1"
+  value="$2"
+
+  if grep -q "^$key" "$SSHD"; then
+    sed -i "s/^$key.*/$key $value/" "$SSHD"
+  else
+    echo "$key $value" >> "$SSHD"
+  fi
+}
+
+apply PermitRootLogin no
+apply X11Forwarding no
+apply ClientAliveInterval 300
+apply ClientAliveCountMax 2
+
+# ตรวจ config ก่อน restart (สำคัญมาก)
+sshd -t
 
 systemctl restart ssh
